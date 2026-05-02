eda415b2bef6d7007910482ffcb706f0dca1471d
Aatif <maatif881@gmail.com>
2026-05-02 15:40:38 +0530

    Feat: Analytics api done

---
+++ backend/src/routes/analytics.route.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { summaryAnalyticsController } from "../controllers/analytics.controller";
+
+
+
+
+
+const analyticsRoutes = Router();
+
+analyticsRoutes.get("/summary", summaryAnalyticsController);
+
+
+export default analyticsRoutes;
--- backend/src/routes/transaction.route.ts
+++ backend/src/routes/transaction.route.ts
@@ -1,16 +1,51 @@
-import { Router } from "express"
-import { bulkDeleteTransactionController, createTransactionController, deleteTransactionController, duplicateTransactionController, getAllTransactionController, getTransactionByIdController } from "../controllers/transaction.controller";
+// import { Router } from "express"
+// import { bulkDeleteTransactionController, createTransactionController, deleteTransactionController, duplicateTransactionController, getAllTransactionController, getTransactionByIdController } from "../controllers/transaction.controller";
 
 
-const transactionRouter = Router();
+// const transactionRouter = Router();
+
+// transactionRouter.post('/create', createTransactionController);
+// transactionRouter.get('/all', getAllTransactionController);
+// transactionRouter.get('/:id', getTransactionByIdController);
+// transactionRouter.put("/duplicate/:id", duplicateTransactionController);
+// transactionRouter.delete("/bulk-delete", bulkDeleteTransactionController);
+// transactionRouter.delete("/delete/:id", deleteTransactionController);
+
+// export default transactionRouter;
 
 
+import { Router } from "express";
+import {
+  bulkDeleteTransactionController,
+  bulkTransactionController,
+  createTransactionController,
+  deleteTransactionController,
+  duplicateTransactionController,
+  getAllTransactionController,
+  getTransactionByIdController,
+  scanReceiptController,
+  updateTransactionController,
+} from "../controllers/transaction.controller";
+import { upload } from "../config/cloudinary.config";
 
-transactionRouter.post('/create', createTransactionController);
-transactionRouter.get('/all', getAllTransactionController);
-transactionRouter.get('/:id', getTransactionByIdController);
-transactionRouter.get("/duplicate/:id", duplicateTransactionController);
-transactionRouter.delete("/bulk-delete", bulkDeleteTransactionController);
-transactionRouter.delete("/delete/:id", deleteTransactionController);
+const transactionRoutes = Router();
 
-export default transactionRouter;
+transactionRoutes.post("/create", createTransactionController);
+
+transactionRoutes.post(
+  "/scan-receipt",
+  upload.single("receipt"),
+  scanReceiptController
+);
+
+transactionRoutes.post("/bulk-transaction", bulkTransactionController);
+
+transactionRoutes.put("/duplicate/:id", duplicateTransactionController);
+transactionRoutes.put("/update/:id", updateTransactionController);
+
+transactionRoutes.get("/all", getAllTransactionController);
+transactionRoutes.get("/:id", getTransactionByIdController);
+transactionRoutes.delete("/delete/:id", deleteTransactionController);
+transactionRoutes.delete("/bulk-delete", bulkDeleteTransactionController);
+
+export default transactionRoutes;
