efd1e5c3217f6c852b86a447856b68d73359d379
Aatif <maatif881@gmail.com>
2026-06-24 11:09:35 +0530

    code cleanup

--- backend/src/services/transaction.service.ts
+++ backend/src/services/transaction.service.ts
@@ -264,66 +264,6 @@ export const bulkTransactionService = async (
 
 
 
-// export const scanReceiptService = async (
-//   file: Express.Multer.File | undefined
-// ) => {
-//   if (!file) throw new BadRequestException("No file uploaded");
-
-//   try {
-//     if (!file.path) throw new BadRequestException("failed to upload file");
-
-//     console.log(file.path);
-
-//     const responseData = await axios.get(file.path, {
-//       responseType: "arraybuffer",
-//     });
-//     const base64String = Buffer.from(responseData.data).toString("base64");
-
-//     if (!base64String) throw new BadRequestException("Could not process file");
-
-//     const result = await genAI.models.generateContent({
-//       model: genAIModel,
-//       contents: [
-//         createUserContent([
-//           receiptPrompt,
-//           createPartFromBase64(base64String, file.mimetype),
-//         ]),
-//       ],
-//       config: {
-//         temperature: 0,
-//         topP: 1,
-//         responseMimeType: "application/json",
-//       },
-//     });
-
-//     const response = result.text;
-//     const cleanedText = response?.replace(/```(?:json)?\n?/g, "").trim();
-
-//     if (!cleanedText)
-//       return {
-//         error: "Could not read reciept  content",
-//       };
-
-//     const data = JSON.parse(cleanedText);
-
-//     if (!data.amount || !data.date) {
-//       return { error: "Reciept missing required information" };
-//     }
-
-//     return {
-//       title: data.title || "Receipt",
-//       amount: data.amount,
-//       date: data.date,
-//       description: data.description,
-//       category: data.category,
-//       paymentMethod: data.paymentMethod,
-//       type: data.type,
-//       receiptUrl: file.path,
-//     };
-//   } catch (error) {
-//     return { error: "Reciept scanning  service unavailable" };
-//   }
-// };
 
 export const scanReceiptService = async (file: Express.Multer.File | undefined) => {
   if (!file) throw new BadRequestException("No file uploaded");
